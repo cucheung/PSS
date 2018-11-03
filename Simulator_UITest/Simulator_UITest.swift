15899e5246dd7a9230577db7ccf9e2b6c82a970f
Curtis Cheung <curtisshing202@gmail.com>
2018-11-03 12:33:36 -0700

    Split Up Simulator and Physical Device UITests

---
+++ Simulator_UITest/Simulator_UITest.swift
@@ -0,0 +1,57 @@
+//  PSSUITests.swift
+//  PSSUITests
+
+//  Description: UI Test Cases for PSS
+
+//  CMPT 275 Group 3 - SavePark
+//  Fall 2018
+
+//  File Created By: Curtis Cheung
+//  File Modified By: Curtis Cheung, David Ling
+
+//  Changes:
+//  10/20/2018 - Added dummy XCTAssertFalse test for Travis CI
+//  10/30/2018 - Added button testing
+
+import XCTest
+
+class PSSUITests: XCTestCase {
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        let app = XCUIApplication()
+        app.launch()
+        
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testNoCameraAccess() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        
+        let app = XCUIApplication()
+        app.buttons["Photo Mode"].tap()
+        app.alerts["Error"].buttons["OK"].tap()
+        XCTAssertFalse(false)
+    }
+    
+    func testGalleryModeLaunch() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        app.buttons["Gallery Mode"].tap()
+        app.buttons["Back"].tap()
+        XCTAssertFalse(false)
+    }
+}
