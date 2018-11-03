1eb045ff0f1a7a529ff74c9d1d45e8d56951423d
Curtis Cheung <curtisshing202@gmail.com>
2018-11-03 12:54:59 -0700

    Rebuilt iPhone UI Tests

--- PSSUITests_iPhone/PSSUITests_iPhone.swift
+++ PSSUITests_iPhone/PSSUITests_iPhone.swift
@@ -16,20 +16,20 @@
 import XCTest
 
 class PSSUITests: XCTestCase {
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         let app = XCUIApplication()
         app.launch()
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
@@ -42,7 +42,7 @@ class PSSUITests: XCTestCase {
         let app = XCUIApplication()
         app.buttons["Photo Mode"].tap()
         app.alerts["Error"].buttons["OK"].tap()
-
+        
         
         
         XCTAssertFalse(false)
@@ -56,7 +56,7 @@ class PSSUITests: XCTestCase {
         app.alerts["Instructions"].buttons["OK"].tap()
         app.buttons["Back"].tap()
         
-
+        
         XCTAssertFalse(false)
     }
     
@@ -88,10 +88,10 @@ class PSSUITests: XCTestCase {
         okButton.tap()
         app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .button).element.tap()
         okButton.tap()
-
+        
         app.buttons["Save"].tap()
-
-
+        
+        
         XCTAssertFalse(false)
     }
     
@@ -111,8 +111,8 @@ class PSSUITests: XCTestCase {
         
         app.buttons["Save"].tap()
         app.alerts["Allow “PSS” to delete this photo?"].buttons["Delete"].tap()
-    
-
+        
+        
         XCTAssertFalse(false)
     }
     
@@ -127,8 +127,8 @@ class PSSUITests: XCTestCase {
         app.buttons["Flash Off Icon"].tap()
         app.buttons["Flash On Icon"].tap()
         
-
-    
+        
+        
         XCTAssertFalse(false)
     }
     
@@ -143,7 +143,7 @@ class PSSUITests: XCTestCase {
         app.buttons["Rear Camera Icon"].tap()
         app.buttons["Front Camera Icon"].tap()
         
-   
+        
         XCTAssertFalse(false)
     }
     
@@ -155,7 +155,7 @@ class PSSUITests: XCTestCase {
         app.buttons["Gallery Mode"].tap()
         app.buttons["Back"].tap()
         
-
+        
         
         XCTAssertFalse(false)
     }
@@ -163,7 +163,7 @@ class PSSUITests: XCTestCase {
     func testGalleryModeShare() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-
+        
         
         let app = XCUIApplication()
         app.buttons["Gallery Mode"].tap()
@@ -171,7 +171,7 @@ class PSSUITests: XCTestCase {
         let collectionViewsQuery = app.collectionViews
         collectionViewsQuery.children(matching: .cell).element(boundBy: 0).tap()
         XCUIApplication().buttons["Share"].tap()
-
+        
         XCTAssertFalse(false)
     }
     
@@ -197,7 +197,7 @@ class PSSUITests: XCTestCase {
         app.collectionViews.children(matching: .cell).element(boundBy: 0).tap()
         app.buttons["Delete"].tap()
         
-
+        
         XCTAssertFalse(false)
     }
 }
