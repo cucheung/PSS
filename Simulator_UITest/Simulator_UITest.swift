3f20407e08082a778de5a9e59bcbd2488ca918e5
Curtis Cheung <curtisshing202@gmail.com>
2018-11-04 11:06:46 -0800

    Changes: Modified Travis script to only run Simulator_UITests Modified UITest platform for both tests to be iOS isntead of only iphone or iosimulator Renamed Simulator_UITest class to reflect filename

--- Simulator_UITest/Simulator_UITest.swift
+++ Simulator_UITest/Simulator_UITest.swift
@@ -15,7 +15,7 @@
 
 import XCTest
 
-class PSSUITests: XCTestCase {
+class Simulator_UITest: XCTestCase {
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
