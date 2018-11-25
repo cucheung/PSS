4931a7e2dcfb0e74885ef5234a7f6267e36bff38
Curtis Cheung <curtisshing202@gmail.com>
2018-11-25 12:10:54 -0800

    Add Sample Video + Removed testing code in PhotoMode.swift to fix NoCameraAccess test

--- PhotoMode/PhotoMode.swift
+++ PhotoMode/PhotoMode.swift
@@ -108,8 +108,8 @@ extension ViewController {
             cameraController.prepare {(error) in
                 if let error = error {
                     print(error)
-                    cameraDetected = true;
-                    //self.dismiss(animated: true)
+                    cameraDetected = false;
+                    self.dismiss(animated: true)
                 }
                 try? self.cameraController.displayPreview(on: self.capturePreviewView)
             }
