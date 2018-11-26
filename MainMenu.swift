af4de08e37e42e7b8bcfe2d1b3e9c27c8b1a5e9f
Curtis Cheung <curtisshing202@gmail.com>
2018-11-25 16:39:55 -0800

    Added Tutorial Video Button (plays placeholder video) in Main Menu

---
+++ MainMenu.swift
@@ -0,0 +1,39 @@
+//  MainMenu.swift
+
+//  Description: Main Menu of PSS
+
+//  CMPT 275 Group 3 - SavePark
+//  Fall 2018
+
+//  File Created By: Curtis Cheung
+//  File Modified By: Curtis Cheung
+
+//  All changes are marked with "CMPT275" (no quotes)
+//  Changes:
+//  11/25/2018 - Created (Added Video Player functionality)
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class MainMenu: UIViewController {
+    
+    @IBOutlet weak var VideoPlayer: UIButton!
+    
+    //  "Video" button in Main Menu
+    //  Input: User presses "Video" button
+    //  Output: Video Player of Tutorial Video
+    @IBAction func VideoPlayer(_ sender: Any) {
+        guard let path = Bundle.main.path(forResource: "sample", ofType:"mp4") else {
+            debugPrint("sample.mp4 not found")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        self.present(playerViewController, animated: true) {
+            playerViewController.player!.play()
+        }
+    }
+    
+}
